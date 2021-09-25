25b29833b5afccbffa07ee88235b71c4edfabaaa
SunBoy <zxm@qq.com>
2021-09-25 16:39:56 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git!!!!!");
         System.out.println("hello git33333!!!!!");
         System.out.println("hello git33333!!!!!");
+        System.out.println("hot-fix test!!");
     }
 }
